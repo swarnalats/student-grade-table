94939429ba73ff8c3a3aa7dd894e145c587c50da
swarnalats <swarna_latha@hotmail.com>
2020-05-12 19:52:13 -0700

    Completed view-average-grade and add-a-grade features of SGT

--- components/app.js
+++ components/app.js
@@ -9,15 +9,19 @@ class App {
         var totalGrade = 0;
         for(var i=0; i< grades.length ;i++)  
             totalGrade += grades[i].grade;
-        average = totalGrade/3;
+        average = totalGrade/i;
         this.pageHeader.updateAverage(average);
     }
 
-    constructor(gradeTable, pageHeader){
+    constructor(gradeTable, pageHeader,gradeForm){
         this.gradeTable = gradeTable;
         this.pageHeader = pageHeader;
+        this.gradeForm = gradeForm;
         this.handleGetGradesError = this.handleGetGradesError.bind(this);
         this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+        this.createGrade = this.createGrade.bind(this);
+        this.handleCreateGradeError = this.handleGetGradesError.bind(this);
+        this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
     }
 
     getGrades() {
@@ -33,8 +37,35 @@ class App {
             });  
  
     }
-    
+    postGrades(name, course, grade) {
+        $.ajax({
+            method:"POST",
+            url:"https://sgt.lfzprototypes.com/api/grades",
+            data:
+            {
+                "name": name,
+                "course": course,
+                "grade": grade
+            },
+            headers:
+                    {
+                        "X-Access-Token": "l7M4qpGu"
+            },
+            success:this.handleCreateGradesSuccess,
+            error:this.handleCreteGradesError, 
+        });
+    }
+    createGrade(name, course, grade){
+        this.postGrades(name, course, grade);
+    }
+    handleCreateGradeError(error){
+        console.error(error);
+    }
+    handleCreateGradeSuccess(){
+        this.getGrades();
+    }    
     start(){
         this.getGrades();
+        this.gradeForm.onSubmit(this.createGrade);
     }
 }
--- main.js
+++ main.js
@@ -2,5 +2,7 @@ var studentGradeTableId = document.getElementById('sgt');
 var gradeTable = new GradeTable(studentGradeTableId); 
 var pageHeaderId = document.getElementById('pageheader');
 var pageHeader = new PageHeader(pageHeaderId);
-const app = new App(gradeTable, pageHeader);
+var formElementId = document.getElementById('studentform'); 
+var formElement = new GradeForm(formElementId);
+const app = new App(gradeTable, pageHeader,formElement);
 app.start();
